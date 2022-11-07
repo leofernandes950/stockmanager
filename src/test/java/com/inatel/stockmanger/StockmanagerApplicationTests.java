66f1210d2bfeff9cbe7cc0c2e00b986b661f9a0c
Leonardo Fernandes <leofernandes95.lf@gmail.com>
2022-11-07 18:48:36 -0300

    Initial Commit

---
+++ src/test/java/com/inatel/stockmanger/StockmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inatel.stockmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
